23195a14ca1722c148fcda0a3bf283a91a5b1332
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-15 09:49:35 -0400

    Done, for now

--- app/controllers/number_controller.rb
+++ app/controllers/number_controller.rb
@@ -3,5 +3,19 @@ class NumberController < ApplicationController
   end
 
   def show
+  	@math = HTTParty.get("http://numbersapi.com/#{params[:id]}/math?json").parsed_response
+  	@trivia = HTTParty.get("http://numbersapi.com/#{params[:id]}/trivia?json").parsed_response
+  	@date = HTTParty.get("http://numbersapi.com/#{params[:id]}/date?json").parsed_response
   end
+
+  def search
+	@student = Student.find_by_name params[:search_name]
+	render action: 'show'
+  end
+
+
+  private
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'number/index'
+  get 'number/index' => 'number#index'
 
-  get 'number/show'
+  get 'number/:id' => 'number#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
